f166b08731a3f4c82b21e8908058a51a36987f0b
tagp <thomas@galehuset.org>
2018-03-02 11:26:27 +0100

    Init

---
+++ src/LinkedList.java
@@ -0,0 +1,254 @@
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class LinkedList<E> implements IList<E>{
+    /**
+     * ,* Gir det første elementet i listen.
+     * ,*
+     * ,* @return Det første elementet i listen.
+     * ,* @throws NoSuchElementException Hvis listen er tom.
+     * ,
+     */
+    @Override
+    public E first() throws NoSuchElementException {
+        return null;
+    }
+
+    /**
+     * ,* Returnerer alle elementene i listen bortsett fra det
+     * ,* første.
+     * ,*
+     * ,* @return Resten av listen.
+     * ,
+     */
+    @Override
+    public IList<E> rest() {
+        return null;
+    }
+
+    /**
+     * ,* Legger til et element på slutten av listen.
+     * ,
+     *
+     * @param elem
+     */
+    @Override
+    public boolean add(E elem) {
+        return false;
+    }
+
+    /**
+     * ,* Legger til et element på begynnelsen av listen.
+     * ,
+     *
+     * @param elem
+     */
+    @Override
+    public boolean put(E elem) {
+        return false;
+    }
+
+    /**
+     * ,* Fjerner det første elementet i listen.
+     * ,*
+     * ,* @return Det første elementet i listen.
+     * ,* @throws NoSuchElementException Hvis listen er tom.
+     * ,
+     */
+    @Override
+    public E remove() throws NoSuchElementException {
+        return null;
+    }
+
+    /**
+     * ,* Fjerner det angitte objektet fra listen.
+     * ,*
+     * ,* @param o Objektet som skal fjernes.
+     * ,* @return true hvis et element ble fjernet, false
+     * ,* ellers.
+     * ,
+     *
+     * @param o
+     */
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    /**
+     * ,* Sjekker om et element er i listen.
+     * ,*
+     * ,* @param o objektet vi sjekker om er i listen.
+     * ,* @return true hvis objektet er i listen, false ellers.
+     * ,
+     *
+     * @param o
+     */
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    /**
+     * ,* Sjekker om listen er tom.
+     * ,*
+     * ,* @return true hvis listen er tom, false ellers.
+     * ,
+     */
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    /**
+     * ,* Legger til alle elementene i den angitte listen på
+     * ,* slutten av listen.
+     * ,*
+     * ,* @param listen som blir lagt til.
+     * ,
+     *
+     * @param list
+     */
+    @Override
+    public void append(IList<? super E> list) {
+
+    }
+
+    /**
+     * ,* Legger til alle elementene i den angitte listen på
+     * ,* begynnelsen av listen.
+     * ,*
+     * ,* @param list listen som blir lagt til
+     * ,
+     *
+     * @param list
+     */
+    @Override
+    public void prepend(IList<? super E> list) {
+
+    }
+
+    /**
+     * ,* Slår sammen flere lister
+     * ,*
+     * ,* @param lists listene som skal slås sammen
+     * ,* @return Ny liste med alle elementene fra listene som
+     * ,* skal slås sammen.
+     * ,
+     *
+     * @param lists
+     */
+    @Override
+    public IList<E> concat(IList<? super E>... lists) {
+        return null;
+    }
+
+    /**
+     * ,* Sorterer listen ved hjelp av en
+     * ,* sammenligningsfunksjon
+     * ,* @param c sammenligningsfunksjon som angir rekkefølgen
+     * ,* til elementene i listen
+     * ,
+     *
+     * @param c
+     */
+    @Override
+    public void sort(Comparator<? super E> c) {
+
+    }
+
+    /**
+     * ,* Fjerner elementer fra listen som svarer til et
+     * ,* predikat.
+     * ,* @param filter predikat som beskriver hvilken
+     * ,* elementer som skal fjernes.
+     * ,
+     *
+     * @param filter
+     */
+    @Override
+    public void filter(Predicate<? super E> filter) {
+
+    }
+
+    /**
+     * ,* Kjører en funksjon over hvert element i listen
+     * ,*
+     * ,* @param f en funksjon fra typen til elementene i
+     * ,* listen til en annen type
+     * ,* @return En liste over elementene som funksjonen
+     * ,* returnerer
+     * ,
+     *
+     * @param f
+     */
+    @Override
+    public <U> IList<U> map(Function<? super E, ? extends U> f) {
+        return null;
+    }
+
+    /**
+     * ,* Slår sammen alle elementene i listen ved hjelp av en
+     * ,* kombinasjonsfunksjon.
+     * ,*
+     * ,* @param t Det første elementet i sammenslåingen
+     * ,* @param accum Funksjonen som holder styr på de
+     * ,* sammenslåtte elementene
+     * ,* @param combiner funksjonen som slår sammen to
+     * ,* elementer
+     * ,* @return Den akkumulerte verdien av sammenslåingene
+     * ,
+     *
+     * @param t
+     * @param f
+     */
+    @Override
+    public <T> T reduce(T t, BiFunction<T, ? super E, T> f) {
+        return null;
+    }
+
+    /**
+     * ,* Gir størrelsen på listen
+     * ,*
+     * ,* @return Størrelsen på listen
+     * ,
+     */
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    /**
+     * ,* Fjerner alle elementene i listen.
+     * ,
+     */
+    @Override
+    public void clear() {
+
+    }
+
+    /**
+     * Returns an iterator over elements of type {@code T}.
+     *
+     * @return an Iterator.
+     */
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+
+                return false;
+            }
+
+            @Override
+            public E next() {
+                return null;
+            }
+        };
+    }
+}
---
+++ src/ListTest.java
@@ -0,0 +1,222 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class ListTest {
+    IList<Integer> myList;
+
+    @BeforeEach
+    void setup(){
+        myList = new LinkedList<Integer>();
+    }
+    // Tester for deloppg 1.1 - 0 elements
+    @Test
+    void test_first_zero(){
+
+    }
+
+    @Test
+    void test_rest_zero(){
+
+    }
+
+    @Test
+    void test_put_zero(){
+
+    }
+
+    @Test
+    void test_remove_zero(){
+
+    }
+
+    // Test for deloppg 1.2 - 1 element
+    @Test
+    void test_first_one(){
+
+    }
+
+    @Test
+    void test_rest_one(){
+
+    }
+
+    @Test
+    void test_put_one(){
+
+    }
+
+    @Test
+    void test_remove_one(){
+
+    }
+
+    // Test for deloppg 1.3 - 2 or more elements
+    @Test
+    void test_first_more(){
+
+    }
+
+    @Test
+    void test_rest_more(){
+
+    }
+
+    @Test
+    void test_put_more(){
+
+    }
+
+    @Test
+    void test_remove_more(){
+
+    }
+
+    @Test
+    void oppg8_sortIntegers() {
+        // Se oppgave 8
+        IList<Integer> list = new LinkedList<>();
+        List<Integer> values = Arrays.asList(3, 8, 4, 7, 10, 6, 1, 2, 9, 5);
+
+        for (Integer value : values) {
+            list.add(value);
+        }
+        list.sort(Comparator.comparingInt(x -> x));
+
+
+        int n = list.remove();
+        while (list.size() > 0) {
+            int m = list.remove();
+            if (n > m) {
+                fail("Integer list is not sorted.");
+            }
+            n = m;
+        }
+
+    }
+    @Test
+    void oppg8_sortStrings() {
+        // Se oppgave 8
+        IList<String> list = new LinkedList<>();
+        List<String> values = Arrays.asList(
+                "g", "f", "a", "c", "b", "d", "e", "i", "j", "h"
+        );
+        for (String value : values) {
+            list.add(value);
+        }
+
+        list.sort(Comparator.naturalOrder());
+
+        String n = list.remove();
+        while (list.size() > 0) {
+            String m = list.remove();
+            if (n.compareTo(m) > 0) {
+                fail("String list is not sorted.");
+            }
+
+        }
+    }
+
+    @Test
+    void oppg9_filter() {
+        // Se oppgave 9
+        List<Integer> values = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        IList<Integer> list = new LinkedList<>();
+        for (Integer value : values) {
+            list.add(value);
+        }
+
+        list.filter(n -> n % 2 == 1);
+
+        int n = list.remove();
+        while(list.size() > 0) {
+            if (n % 2 == 1) {
+                fail("List contains filtered out elements.");
+            }
+            n = list.remove();
+
+        }
+
+    }
+
+    @Test
+    void oppg10_map() {
+        // Se oppgave 10
+        List<String> values = Arrays.asList("1", "2", "3", "4", "5");
+
+        IList<String> list = new LinkedList<>();
+        for (String value : values) {
+            list.add(value);
+        }
+
+        IList<Integer> result = list.map(Integer::parseInt);
+
+        List<Integer> target = Arrays.asList(1, 2, 3, 4, 5);
+
+        int n = result.remove();
+        for (Integer t : target) {
+            if (n != t) {
+                fail("Result of map gives the wrong value.");
+            }
+        }
+    }
+
+    @Test
+    void oppg11_reduceInts() {
+        // Se oppgave 11
+        List<Integer> values = Arrays.asList(1, 2, 3, 4, 5);
+
+        IList<Integer> list = new LinkedList<>();
+        for (Integer value : values) {
+            list.add(value);
+        }
+
+        int result = list.reduce(0, Integer::sum);
+
+        assertEquals(result, 5*((1 + 5)/2));
+    }
+
+    @Test
+    void oppg11_reduceStrings() {
+        List<String> values = Arrays.asList("e", "s", "t");
+        IList<String> list = new LinkedList<>();
+        for (String s : values) {
+            list.add(s);
+        }
+
+        String result = list.reduce("t", (acc, s) -> acc + s);
+
+        assertEquals(result, "test");
+    }
+
+    @Test
+    void ex1_FastSort() {
+        // Se ekstraoppgave 1
+        Random r = new Random();
+        IList<Integer> list = new LinkedList<>();
+        for (int n = 0; n < 1000000; n++) {
+            list.add(r.nextInt());
+        }
+
+        assertTimeout(Duration.ofSeconds(2), () -> list.sort(Integer::compare));
+
+        int n = list.remove();
+        for(int m = list.remove(); !list.isEmpty(); n = m) {
+            if (n > m) {
+                fail("List is not sorted");
+            }
+        }
+
+
+    }
+}
